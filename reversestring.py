2a6216653cbf29366e2b384004d83a746dfe98ff
SSharnitha <46617496+SSharnitha@users.noreply.github.com>
2019-07-09 19:49:56 +0530

    Update reversestring.py

--- reversestring.py
+++ reversestring.py
@@ -1,8 +1,13 @@
 s=input()
 s=list(s)
 s1=""
-n1=len(s)-1
-for i in range(0,len(s)//2+1):
+n=len(s)
+n1=n-1
+if(n%2==0):
+    n=len(s)//2
+else:
+    n=len(s)//2+1
+for i in range(0,n):
     temp=s[i]
     s[i]=s[n1-i]
     s[n1-i]=temp
