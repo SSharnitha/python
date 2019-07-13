f84f39b07bc9df94b81988d01768215634d0b25c
SSharnitha <46617496+SSharnitha@users.noreply.github.com>
2019-07-13 15:23:05 +0530

    Update costOfInsertion.py

--- costOfInsertion.py
+++ costOfInsertion.py
@@ -1,11 +1,11 @@
-d,a=map(s,input().split())
-e1=0
-if len(d)>len(a):
-   d,a=a,d
-r1=0
-while r1<len(d):
-   e1+=(ord(a[r])-ord(d[r]))
-   r1+=1
-for r1 in range(r1,len(a)):
-   e1+=ord(a[r])-ord('a')+1
-print(e1)
+w,w1=map(str,input().split())
+x1=0
+if len(w)>len(w1):
+   w,w1=w1,w
+s1=0
+while s1<len(w):
+   x1+=(ord(w[s1])-ord(w[s1]))
+   s1+=1
+for s1 in range(s1,len(w1)):
+   x1+=ord(w1[s1])-ord('a')+1
+print(x1)
